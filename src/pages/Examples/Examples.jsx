b17e91e8520ab2e56b77237489a7e62a2eb31e69
Abhishek <abhishek.sawant@simformsolutions.com>
2023-03-13 16:04:26 +0530

    On Click Trigger

--- src/pages/Examples/Examples.jsx
+++ src/pages/Examples/Examples.jsx
@@ -1,5 +1,6 @@
 import Container from '../../components/Container/Container';
 import { Link } from 'react-router-dom';
+import { exampleroutes } from '../../exampleroutes';
 import './Examples.scss';
 
 function Examples() {
@@ -7,9 +8,14 @@ function Examples() {
     <Container className='examples basic__page'>
       <h1>Examples</h1>
       <div className='links'>
-        <Link className='links__item' to='/examples/animatedcard'>
-          Animated Card
-        </Link>
+        {exampleroutes?.map((link, index) => {
+          const { name, path } = link;
+          return (
+            <Link key={index} className='links__item' to={path}>
+              {name}
+            </Link>
+          );
+        })}
       </div>
     </Container>
   );
